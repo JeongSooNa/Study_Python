9eda470a6eac35fa413c6cd1f9a777466e4a73e1
JeongSooNa <skwjdtn3171@naver.com>
2022-09-05 17:58:51 +0900

    feat js

--- pygame/SquidGame/GlassBridge.py
+++ pygame/SquidGame/GlassBridge.py
@@ -7,11 +7,6 @@ life = int(input("Survived Number : "))
 stageNum = 5
 
 for i in range(stageNum) :
-    # end game
-    if life == 0 :
-        print("Game Over..")
-    if i == (stageNum-1) :
-        print("Game Success!!")
 
     ran_num = random.uniform(0,1)
 
@@ -23,7 +18,17 @@ for i in range(stageNum) :
 
     choice = input("your choice (R/L) : ")
     if choice == answer :
-        continue
+        life = life
     else :
         life -= 1
-        continue
+
+    # end game
+    if life == 0 :
+        print("Game Over..")
+        break
+    if i == (stageNum-1) :
+        print("Game Success!!")
+        break
+
+# This logic needs detailed setting
+# ex) member's number / unexpected fighting / other situation
